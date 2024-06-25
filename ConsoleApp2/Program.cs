f24adeeb2e38a3ccaa24f8c25f4de2dc504705ec
manap <Manap1995@mail.ru>
2024-06-25 20:59:43 +0300

    Изменения добавить

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string hello = Console.ReadLine();
+            Console.WriteLine(hello);
+
         }
     }
 }
